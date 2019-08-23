47aa93f9e2ad070fee9f64fea7f5c1e63b8a03c4
ThetaQing <csusq2016@163.com>
2019-08-23 16:47:31 +0800

    change the rayleigh part and add the salt & pepper part to make them visually.

--- Chapter5/example5_2.m
+++ Chapter5/example5_2.m
@@ -3,43 +3,58 @@
 clear, clc
 
 % gaussian
-r = imnoise2('gaussian', 100000, 1, 0, 1);
+r = imnoise2('gaussian', 100000, 1);
 p = hist(r, 50);
 subplot(2,3,1)
 bar(p)
 title('gaussian')
+% xlim([ -5 5])
 
 % uniform
-r = imnoise2('uniform', 100000, 1, 0, 1);
+r = imnoise2('uniform', 100000, 1);
 p = hist(r, 50);
 subplot(2,3,2)
 bar(p)
 title('uniform')
+% xlim([0 1])
 
-% salt & pepper
-r = imnoise2('salt & pepper', 20, 20, 0.05, 0.05);
+
+% lognormal
+r = imnoise2('lognormal', 100000, 1);
 p = hist(r, 50);
 subplot(2,3,3)
 bar(p)
-title('salt&pepper')
+title('lognormal')
+% xlim([0 3.5])
 
-% lognormal
-r = imnoise2('lognormal', 100000, 1, 1, 0.25);
+% rayleigh
+r = imnoise2('rayleigh', 1, 10000, 0, 8);
 p = hist(r, 50);
 subplot(2,3,4)
 bar(p)
-title('lognormal')
+title('rayleigh')
+% xlim([0 3.5])
 
-% rayleigh
-r = imnoise2('rayleigh', 20, 20, 1, 4);
+% exponential
+r = imnoise2('exponential', 100000, 1);
 p = hist(r, 50);
 subplot(2,3,5)
 bar(p)
-title('raleigh')
+title('exponential')
+% xlim([0 14])
 
 % erlang
-r = imnoise2('erlang', 100000, 1, 2, 5);
+r = imnoise2('erlang', 100000, 1);
 p = hist(r, 50);
 subplot(2,3,6)
 bar(p)
 title('erlang')
+% xlim([0 12])
+
+% erlang
+r = imnoise2('salt & pepper', 1, 1000);
+p = hist(r, 50);
+figure('name', 'salt & pepper')
+bar(p)
+title('salt & pepper')
+% xlim([0 12])
